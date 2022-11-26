7b5c8acfd25c103bb44627f9922f9d9514c67dc4
Bintang <55781680+ykbintang@users.noreply.github.com>
2022-11-26 20:12:13 +0700

    Add files via upload

---
+++ application/views/about_us.php
@@ -0,0 +1,78 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 mb-4 pt-5">
+            <h4 class="text-purple pb-3 text-center">About Us</h4>
+            <p class="paragraph text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+                irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+                anim id est laborum.</p>
+        </div>
+        
+        <div class="col mb-4 text-center">
+            <h4 class="text-purple">About Developer</h4>
+        </div>
+        <div class="body col-md-12">
+            <div class="wrap">
+                <table border="0">
+                    <tr>
+                        <td rowspan="11" width="100px">
+                            <img class="profil" src="<?=base_url('images')?>/foto-profil.jpg" alt="foto profil" width="200px">
+                        </td>
+                    </tr>
+                    <tr>
+                        <th colspan="3">
+                            <h1 align="center">BIODATA DIRI</h1>
+                        </th>
+                    </tr>
+                    <tr>
+                        <td colspan="3">
+                            <hr align="center" width="30%" style="border-top:black 3px double;">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Nama</td>
+                        <td>:</td>
+                        <td>Yayes Kasnanda Bintang</td>
+                    </tr>
+                    <tr>
+                        <td>NIM</td>
+                        <td>:</td>
+                        <td>L200190206</td>
+                    </tr>
+                    <tr>
+                        <td>Prodi</td>
+                        <td>:</td>
+                        <td>Informatika</td>
+                    </tr>
+                    <tr>
+                        <td>Email</td>
+                        <td>:</td>
+                        <td>l200190206@student.ums.ac.id</td>
+                    </tr>
+                    <tr>
+                        <td>TTL</td>
+                        <td>:</td>
+                        <td>Klaten, 13 Mei 2001</td>
+                    </tr>
+                    <tr>
+                        <td>Alamat</td>
+                        <td>:</td>
+                        <td>Sentono, Karangdowo, Klaten</td>
+                    </tr>
+                    <tr>
+                        <td colspan="3">
+                            <hr align="center" width="30%" style="border-top:black 3px double;">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="3" class="motto">
+                            <b>MOTTO</b><br>"kita tidak harus hebat untuk memulai, <br>tapi kita harus memulai untuk menjadi orang hebat."
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ application/views/home.php
@@ -0,0 +1,69 @@
+<div class="jumbotron">
+	<h1 class="display-4">Selamat datang di <span class="text-purple text-bold">Moedjito</span>Hotel.com</h1>
+	<p class="lead">Website booking hotel dan villa yang mudah, aman, terpercaya.</p>
+	<a class="btn btn-primary btn-lg" href="<?=base_url('order/apartements')?>" role="button">Pesan Sekarang</a>
+</div>
+
+<section class="kota-list bg-light pb-2">
+<div class="container">
+	<div class="section-title"><span>Tersedia</span> di berbagai kota besar</div>
+	<div class="row row-cols-1 pt-2">
+		<div class="col mb-4">
+			<div class="card">
+			<img style="width: 250px;" src="<?=base_url('assets')?>/images/bandung.png" class="card-img-top" alt="...">
+			<div class="card-body kota">
+				<h5 class="card-title">Bandung</h5>
+				<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+			</div>
+			</div>
+		</div>
+		<div class="col mb-4">
+			<div class="card">
+			<img style="width: 250px;" src="<?=base_url('assets')?>/images/yogyakarta.png" class="card-img-top" alt="...">
+			<div class="card-body kota">
+				<h5 class="card-title">Yogyakarta</h5>
+				<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+			</div>
+			</div>
+		</div>
+		<div class="col mb-4">
+			<div class="card">
+			<img style="width: 250px;" src="<?=base_url('assets')?>/images/semarang.jpg" class="card-img-top" alt="...">
+			<div class="card-body kota">
+				<h5 class="card-title">Semarang</h5>
+				<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+			</div>
+			</div>
+		</div>
+	</div>
+	<div class="row row-cols-1 pt-2">
+		<div class="col mb-4">
+			<div class="card">
+			<img style="width: 250px;" src="<?=base_url('assets')?>/images/surabaya.png" class="card-img-top" alt="...">
+			<div class="card-body kota">
+				<h5 class="card-title">Surabaya</h5>
+				<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+			</div>
+			</div>
+		</div>
+		<div class="col mb-4">
+			<div class="card">
+			<img style="width: 250px;" src="<?=base_url('assets')?>/images/jakarta.png" class="card-img-top" alt="...">
+			<div class="card-body kota">
+				<h5 class="card-title">Jakarta</h5>
+				<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+			</div>
+			</div>
+		</div>
+		<div class="col mb-4">
+			<div class="card">
+			<img style="width: 250px;" src="<?=base_url('assets')?>/images/bali.png" class="card-img-top" alt="...">
+			<div class="card-body kota">
+				<h5 class="card-title">Bali</h5>
+				<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+			</div>
+			</div>
+		</div>
+	</div>
+</div>
+</section>
